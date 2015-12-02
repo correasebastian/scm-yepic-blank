27c7b3b9156f0e962ae7c31a6b2505ad5afb5ce3
sebastian correa morales <correa.m.sebastian@gmail.com>
2015-12-02 15:12:16 -0500

    ready to start
    from scm-ionic-blank, change name and id,

---
+++ README.md
@@ -0,0 +1,14 @@
+# scm-ionic-blank
+
+ionic blank project with gulp ,wiredep and inject
+
+clone the repo, change the appname and id 
+
+package.json, ionic.project, config.xml
+
+npm install
+
+bower install
+
+
+you are ready
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var $ = require('gulp-load-plugins')({
+    lazy: true
+});
+var config = require('./gulp.config')();
+var paths = {
+    sass: ['./scss/**/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+    gulp.src('./scss/ionic.app.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./www/css/'))
+        .on('end', done);
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('install', ['git-check'], function() {
+    return bower.commands.install()
+        .on('log', function(data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        });
+});
+
+gulp.task('git-check', function(done) {
+    if (!sh.which('git')) {
+        console.log(
+            '  ' + gutil.colors.red('Git is not installed.'),
+            '\n  Git, the version control system, is required to download Ionic.',
+            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+        );
+        process.exit(1);
+    }
+    done();
+});
+
+
+
+// gulp.task('wiredep',['templatecache'], function() {
+gulp.task('wiredep', function() {
+    log('Wire up the bower css js and our app js into the html');
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js, {
+            read: false
+        }), {
+            relative: true
+        }))
+        .pipe(gulp.dest(config.client));
+});
+
+
+
+//////////////////////////////////////////////////////////////////
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
